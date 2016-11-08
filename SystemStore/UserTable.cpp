7344bc30b5459377de704cd33c29175fe40cb655
Xiao Shengguang <shengguangxiao@gmail.com>
2016-11-08 23:57:53 +0800

    Add files via upload

--- SystemStore/UserTable.cpp
+++ SystemStore/UserTable.cpp
@@ -5,6 +5,45 @@ namespace AOI
 {
 namespace SystemStore
 {
+    namespace
+    {
+        Table::FieldEntry const myFields[] =
+        {
+            { SL("id"),                  Table::BIT_INTID | Table::BIT_PKEYINC, SL("") },
+            { SL("name"),                Table::BIT_NCSTR,                      SL("") },
+            { SL("password"),            Table::BIT_NCSTR,                      SL("") },
+            { SL("restriction"),         Table::BIT_NCSTR,                      SL("") },            
+        };
 
+        BOOST_STATIC_ASSERT(sizeof(myFields) / sizeof(myFields[0]) == UserTable::COUNT_);
+
+        String::value_type const *constraints[] =
+        {
+            SL("foreign key (light_id) references light (id)"),
+        };
+    }
+
+    int UserTable::GetFieldBits(int index) const
+    {
+        assert(index >= 0 && index < GetFieldCount() && "Field index out of range in UserTable::GetFieldBits.");
+        return myFields[index].fieldBits;
+    }
+
+    String UserTable::GetFieldName(int index) const
+    {
+        assert(index >= 0 && index < GetFieldCount() && "Field index out of range in UserTable::GetFieldName.");
+        return myFields[index].fieldName;
+    }
+
+    String UserTable::GetFieldSql(int index) const
+    {
+        assert(index >= 0 && index < GetFieldCount() && "Field index out of range in UserTable::GetFieldSql.");
+        return myFields[index].fieldSql;
+    }
+
+    /*static*/String UserTable::StaticGetTableName()
+    {
+        return SL("users");
+    }
 }
 }
--- SystemStore/UserTable.h
+++ SystemStore/UserTable.h
@@ -4,12 +4,34 @@ namespace AOI
 {
 namespace SystemStore
 {
-    class UserTable
+    class UserTable : public Table
     {
     public:
-        UserTable();
-        ~UserTable();
-        static int CreateTable();
+        explicit UserTable(DatabasePtr const &database ) : Table( database )   {}
+        virtual ~UserTable() {};
+        
+        enum FieldIndex
+        {
+            ID,
+            NAME,
+            PASSWORD,
+            RESTRICTION,
+            COUNT_,
+        };
+
+        /********
+        * Table *
+        ********/
+        virtual String GetTableName()    const override { return StaticGetTableName(); }
+        virtual int    GetFieldCount()   const override { return COUNT_; }
+        virtual String GetFieldName(int) const override;
+        virtual int    GetFieldBits(int) const override;
+        virtual String GetFieldSql (int) const override;
+
+        /*************
+        * AlignmentTable *
+        *************/
+        static String StaticGetTableName();
     };
 }
 }
